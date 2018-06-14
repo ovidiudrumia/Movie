36d469fd82ab22da70ed1779175a159f3f9a3f38
ovidiudrumia <ovidiu.drumia@yahoo.com>
2018-06-14 10:49:01 +0300

    Movie Rental Initial commit

---
+++ src/main/java/Rental.java
@@ -0,0 +1,20 @@
+/**
+ * Created by ovidi on 6/14/2018.
+ */
+public class Rental {
+    private Movie movie;
+    private int daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+}
